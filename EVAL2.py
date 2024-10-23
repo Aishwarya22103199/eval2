005498c9e1fdd2f938d7555b2d1ce784f51f214b
Aishwarya22103199 <anuvrata.priya@gmail.com>
2024-10-23 03:46:25 -0700

    Add files via upload

---
+++ EVAL2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install pymongo
+
+
+# In[ ]:
+
+
+import pandas as pd
+from pymongo import MongoClient
+
+#part 3
+df['practicestate'] =
+
+def create_mongodb_sch():
+    client= MongoClient("mongodb://localhost:27017")
+    db=client["Medical_Equipment_Suppliers.db"]
+    collection =
+    data=df.to_dict("")
+    collection_insert_many(data)
+    print("data is succesfully inserted into mongodb.")
+    
+    
+def 
+soup = BeautifulSoup()
+
+
+data[]=
+ for rows in row[1:]:
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
